0b48833d4f382fba52e798b78abe27e315b45657
fabrik12 <fbalarezo@unsa.edu.pe>
2025-09-29 10:14:47 -0500

    Añadir implementación del cálculo de la integral utilizando el método del trapecio.

--- src/trapecio.cpp
+++ src/trapecio.cpp
@@ -26,5 +26,33 @@ double calcularIntegralSecuencial(double a, double b, int n)
 
 int main()
 {
+    // Condiciones iniciales.
+    const double a = 2.0;
+    const double b = 20.0;
+    int n = 1; // Solo 1 trapecio inicialmente.
+
+    double resultadoAnterior = 0.0;
+    double resultadoActual = calcularIntegralSecuencial(a, b, n);
+
+    std::cout << "Iniciando cálculo de la integral..." << std::endl;
+    // Para mejorar la presentación de los números decimales.
+    std::cout << std::fixed << std::setprecision(10);
+
+    // Bucle hasta que la integral converja.
+    while (resultadoAnterior != resultadoActual)
+    {
+        resultadoAnterior = resultadoActual;
+        n *= 2; // para el siguiente paso, duplicamos el número de trapecios.
+        resultadoActual = calcularIntegralSecuencial(a, b, n);
+
+        // Usando setw y left para alinear la salida.
+        std::cout << "Trapecios: " << std::setw(10) << std::left << n
+                  << "| Área: " << resultadoActual << std::endl;
+    }
+
+    std::cout << "\nConvergencia alcanzada." << std::endl;
+    std::cout << "El valor aproximado de la integral es: " << resultadoActual << std::endl;
+    std::cout << "Número de trapecios utilizado: " << n << std::endl;
+
     return 0;
 }
